84e60e53a228dc416af6f27141debd2b0df9c4a4
Jürgen Ratzenböck <juergen.ratzenboeck@jobs.at>
2019-11-06 16:40:03 +0100

    Add CSRF token to page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>jobs.at coding exercise</title>
         <!-- Fonts -->
+
+        <!-- Comment in the following line, if you want to use bootstrap and/or global styles -->
+{{--        <link rel="stylesheet" href="css/app.css">--}}
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
