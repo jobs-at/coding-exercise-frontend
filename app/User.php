bc004c70d3977bca70609b23b0211c2b10771ebb
Nima shahin <nima_general@yahoo.com>
2021-06-15 16:28:56 +0200

    Auth Provider
    Auth Service provider problems

---
+++ app/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+class User extends Authenticatable
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phone_number',
+        'is_superuser',
+        'is_staff',
+        'last_login_at'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+
+    /**
+     * Check users Admin
+     *
+     * @return mixed
+     */
+    public function isSuperUser()
+    {
+        return $this->is_superuser;
+    }
+}
